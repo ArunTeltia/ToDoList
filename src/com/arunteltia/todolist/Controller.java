cbaf926feabba582d64003c45ebc2a60e125170e
ArunTeltia <43555219+ArunTeltia@users.noreply.github.com>
2019-11-15 04:14:15 +0530

    sorted->todolist->deadline

--- src/com/arunteltia/todolist/Controller.java
+++ src/com/arunteltia/todolist/Controller.java
@@ -4,6 +4,7 @@ import com.arunteltia.todolist.DataModel.ToDoData;
 import com.arunteltia.todolist.DataModel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,6 +19,7 @@ import javafx.util.Callback;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -83,8 +85,17 @@ public class Controller {
             }
         });
 
-        todoListView.setItems((ToDoData.getInstance().getTodoItems()));
+        SortedList<TodoItem> sortedList= new SortedList<TodoItem>(ToDoData.getInstance().getTodoItems(), new Comparator<TodoItem>() {
+            @Override
+            public int compare(TodoItem o1, TodoItem o2) {
+                return o1.getDeadline().compareTo(o2.getDeadline());
+            }
+        });
+
+//        todoListView.setItems((ToDoData.getInstance().getTodoItems()));
+        todoListView.setItems(sortedList);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
         todoListView.getSelectionModel().selectFirst();
 
         todoListView.setCellFactory(new Callback<ListView<TodoItem>, ListCell<TodoItem>>() {
